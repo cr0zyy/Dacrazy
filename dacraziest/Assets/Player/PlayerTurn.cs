070325db5a73549628040a74dcf43f3a2c46dbaa
XxmannenxX <markus.lundevik@gmail.com>
2022-08-20 21:32:25 +0200

    y

---
+++ dacraziest/Assets/Player/PlayerTurn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float PlayerSensitivity = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X"); float mouseY = Input.GetAxis("mouse Y");
+
+
+    }
+}
